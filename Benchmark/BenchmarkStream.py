31882bbee67d24778362905af21344fea81a30b8
ArikReuter <arik_reuter@gmx.de>
2024-07-05 12:57:19 +0200

    small fix

--- Benchmark/BenchmarkStream.py
+++ Benchmark/BenchmarkStream.py
@@ -98,15 +98,17 @@ class Benchmark:
 
         self.setup()
 
-        results = []
+        results = {}
 
         assert len(self.models) == len(self.model_specific_data2params_fun_list), "The number of models and the number of data2params functions must be the same"
         for model, data2params_fun in tqdm(list(zip(self.models, self.model_specific_data2params_fun_list))):
             for n_topics in self.n_topics:
-                results.append(self.benchmark_model(
+                r = self.benchmark_model(
                     model = model,
                     n_topics = n_topics
-                ))
+                )
+
+                results[(model, n_topics)] = r
 
                 print(f"Done with model {model} and n_topics {n_topics}")
 
