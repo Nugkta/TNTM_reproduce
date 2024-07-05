ff0204b664778e46f565554f05a21ffe1f1d347d
ArikReuter <arik_reuter@gmx.de>
2024-07-05 18:12:28 +0200

    small import fix

--- Benchmark/OctisDataset2StreamDataset.py
+++ Benchmark/OctisDataset2StreamDataset.py
@@ -1,7 +1,7 @@
 import octis 
-from stream.data_utils.dataset import dataset
+from stream.data_utils.dataset import TMDataset
 
-class OctisDataset2StreamDataset(dataset):
+class OctisDataset2StreamDataset(TMDataset):
     """
     A class that takes an octis dataset and turns it into a stream dataset
     """
@@ -15,7 +15,7 @@ class OctisDataset2StreamDataset(dataset):
             octis_dataset: an octis dataset
         """
         super().__init__()
-        
+
         # make all the attributes of the octis dataset available
 
         for key, value in octis_dataset.__dict__.items():
